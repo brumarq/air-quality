a59f7206ca58aa81c37a728367d3971206c795c5
Bruno Marques <github.com.museum412@passmail.net>
2025-07-18 19:35:30 +0200

    Update SensorDto to handle measurement object

--- backend/src/main/java/com/airquality/backend/adapter/in/consumer/dto/SensorDto.java
+++ backend/src/main/java/com/airquality/backend/adapter/in/consumer/dto/SensorDto.java
@@ -1,5 +1,6 @@
 package com.airquality.backend.adapter.in.consumer.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +8,28 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SensorDto {
     private Integer id;
+    private String name;
     private ParameterDto parameter;
-    private String unit;
-    private Double lastValue;
-    private String lastUpdated;
+    private MeasurementDto measurement;
+    
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class MeasurementDto {
+        private DateTimeDto datetime;
+        private Double value;
+        
+        @Data
+        @NoArgsConstructor
+        @AllArgsConstructor
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        public static class DateTimeDto {
+            private String utc;
+            private String local;
+        }
+    }
 }
