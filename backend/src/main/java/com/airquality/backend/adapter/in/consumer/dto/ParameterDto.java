f005c92468657b5c76b41c392299b2739d8a9ac1
Bruno Marques <github.com.museum412@passmail.net>
2025-07-18 19:35:35 +0200

    Add JsonIgnoreProperties to ParameterDto

--- backend/src/main/java/com/airquality/backend/adapter/in/consumer/dto/ParameterDto.java
+++ backend/src/main/java/com/airquality/backend/adapter/in/consumer/dto/ParameterDto.java
@@ -1,5 +1,6 @@
 package com.airquality.backend.adapter.in.consumer.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ParameterDto {
     private String name;
     private String units;
